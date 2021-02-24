1eec66783791e1f69a9d05635bcaa9e02a557622
saibaken <shelamov.s@gmail.com>
2021-02-24 13:27:24 +0000

    ПрÐерУдалил strinlen из string.h и Ñstring.h из заголовка

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,15 +1,21 @@
 #include "revert_string.h"
-#include <string.h>
 
 void RevertString(char *str)
 {
 	char temp;
+    int strlen = 0;
     int i = 0;
-    for (i = 0; i<strlen(str)/2; i++)
+    char* ptr = str;
+    while (*ptr!='\0')
+    {
+        strlen++;
+        ptr++;
+    }
+    for (i = 0; i<strlen/2; i++)
     {
         temp = str[i];
-        str[i] = str[strlen(str)-i-1];
-        str[strlen(str)-i-1] = temp;
+        str[i] = str[strlen-i-1];
+        str[strlen-i-1] = temp;
     }
 }
 
