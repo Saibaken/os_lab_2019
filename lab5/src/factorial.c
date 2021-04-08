5aab628847f5c9ac114f404c516875eacc5e35b4
saibaken <shelamov.s@gmail.com>
2021-04-08 10:49:47 +0000

    Done

--- lab5/src/factorial.c
+++ lab5/src/factorial.c
@@ -17,16 +17,9 @@ struct fac_thread{
 
 static int module_multiplication(int a, int b, int mod) {
   int result = 0;
-  a = a % mod;
+  result =(a%mod)*(b%mod)%mod;
 
-  while (b > 0) {
-    if (b % 2 == 1)
-      result = (result + a) % mod;
-    a = (a * 2) % mod;
-    b /= 2;
-  }
-
-  return result % mod;
+  return result;
 }
 
 static void fac_threaded(struct fac_thread* f) {
